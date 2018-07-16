e990f0aeeec959a21db7bfafe08f9ce0a0b11be0
petrs <petrsgit@gmail.com>
2018-07-16 18:04:00 +0200

    updated verfied list of well-known AIDs
    verified against exp files, packages up to JC 3.0.5 included

--- src/main/java/pro/javacard/WellKnownAID.java
+++ src/main/java/pro/javacard/WellKnownAID.java
@@ -31,24 +31,29 @@ public class WellKnownAID {
 
     static {
         // Copied from https://stackoverflow.com/questions/25031338/how-to-get-javacard-version-on-card/25063015#25063015
-        // NOT verified
+        // Extended and verified against JC SDK exp files
         registry.put(AID.fromString("A0000000620001"), "java.lang");
         registry.put(AID.fromString("A0000000620002"), "java.io");
         registry.put(AID.fromString("A0000000620003"), "java.rmi");
 
         registry.put(AID.fromString("A0000000620101"), "javacard.framework");
-        registry.put(AID.fromString("A0000000620102"), "javacard.security");
         registry.put(AID.fromString("A000000062010101"), "javacard.framework.service");
+        registry.put(AID.fromString("A0000000620102"), "javacard.security");
 
         registry.put(AID.fromString("A0000000620201"), "javacardx.crypto");
         registry.put(AID.fromString("A0000000620202"), "javacardx.biometry");
         registry.put(AID.fromString("A0000000620203"), "javacardx.external");
-        registry.put(AID.fromString("A0000000620209"), "javacardx.apdu");
+        registry.put(AID.fromString("A0000000620204"), "javacardx.biometry1toN");
+        registry.put(AID.fromString("A0000000620205"), "javacardx.security");
 
         registry.put(AID.fromString("A000000062020801"), "javacardx.framework.util");
         registry.put(AID.fromString("A00000006202080101"), "javacardx.framework.util.intx");
         registry.put(AID.fromString("A000000062020802"), "javacardx.framework.math");
         registry.put(AID.fromString("A000000062020803"), "javacardx.framework.tlv");
+        registry.put(AID.fromString("A000000062020804"), "javacardx.framework.string");
+
+        registry.put(AID.fromString("A0000000620209"), "javacardx.apdu");
+        registry.put(AID.fromString("A000000062020901"), "javacardx.apdu.util");
 
         registry.put(AID.fromString("A00000015100"), "org.globalplatform");
     }
